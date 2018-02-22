574c3ea2d71b11611054aa338fcaf728f4f1d68f
Wei Shuai <cpuwolf@gmail.com>
2018-02-22 12:31:05 +0800

    add windows version info

--- install/install.cmd
+++ install/install.cmd
@@ -8,6 +8,6 @@ cd F:\works\GitHub\pyff777wingflexfix\
 
 ::python.exe -m PyInstaller  --windowed --icon=777.ico --onefile --clean --noconfirm ff777wingflex.py
 
-python.exe -m PyInstaller  --windowed --onefile --clean --noconfirm ff777wingflex.spec
+python.exe -m PyInstaller  --version-file=file_version_info.txt --windowed --onefile --clean --noconfirm ff777wingflex.spec
 
 pause
